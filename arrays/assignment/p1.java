af8029809b2a5dc9d5e350475f62b8c74ac86331
kalyankoduru7-a11y <kalyankoduru7@gmail.com>
2026-09-19 11:34:02 +0530

    Implement Product Except Self algorithm in p1.java

---
+++ arrays/assignment/p1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        
+        // Forward pass: compute prefix products
+        answer[0] = 1;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+        
+        // Backward pass: multiply by suffix products
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+        
+        return answer;
+    }
+}
