21daecacca51ee4282a488af79880e70e153df00
kalyankoduru7-a11y <kalyankoduru7@gmail.com>
2026-09-19 11:17:45 +0530

    Create p1.java

---
+++ stingmutation/class/p1.java
@@ -0,0 +1,23 @@
+public class VowelConsonantCounter {
+    public static void main(String[] args) {
+        countVowelsAndConsonants("Java Programming");
+    }
+
+    public static void countVowelsAndConsonants(String text) {
+        int vowels = 0;
+        int consonants = 0;
+        String lowerText = text.toLowerCase();
+
+        for (int i = 0; i < lowerText.length(); i++) {
+            char c = lowerText.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Vowels: " + vowels + " | Consonants: " + consonants);
+    }
+}
