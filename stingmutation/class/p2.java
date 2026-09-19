b32de9c5169d27fd2479ec93a65a68ea0cb962db
kalyankoduru7-a11y <kalyankoduru7@gmail.com>
2026-09-19 11:18:17 +0530

    Create p2.java

---
+++ stingmutation/class/p2.java
@@ -0,0 +1,18 @@
+public class CSVParser {
+    public static void main(String[] args) {
+        parseStudentRecord("Ananya Verma, RA2211003010123,CSE");
+        parseStudentRecord("Ananya Verma,CSE");
+    }
+
+    public static void parseStudentRecord(String csvLine) {
+        String[] fields = csvLine.split(",");
+        
+        if (fields.length == 3) {
+            System.out.println("Name: " + fields[0].trim() + 
+                               " | Roll No: " + fields[1].trim() + 
+                               " | Dept: " + fields[2].trim());
+        } else {
+            System.out.println("Invalid Record");
+        }
+    }
+}
