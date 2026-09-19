39ff49063aaccd6d19736ac22eba18248e2fc6b3
kalyankoduru7-a11y <kalyankoduru7@gmail.com>
2026-09-19 11:11:19 +0530

    Create p1.java

---
+++ Strings/Assignments/p1.java
@@ -0,0 +1,26 @@
+public class SeatChecker {
+    public static void main(String[] args) {
+        int[] seatsWithDuplicates = {101, 102, 103, 102, 105};
+        int[] seatsWithoutDuplicates = {101, 102, 103, 104, 105};
+        
+        checkDuplicateSeats(seatsWithDuplicates);
+        checkDuplicateSeats(seatsWithoutDuplicates);
+    }
+
+    public static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean duplicateFound = false;
+        
+        for (int i = 0; i < seatNumbers.length; i++) {
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    duplicateFound = true;
+                }
+            }
+        }
+        
+        if (!duplicateFound) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+}
